3829689eceaeb61e70bc78ba94bd09ed4a40a0ef
maksbubata <maks1412@gmail.com>
2025-02-07 15:15:11 +0200

    react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "vite-plugin-svgr";
+const ReactCompilerConfig = {
+  /* ... */
+};
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react(), svgr()],
+  plugins: [
+    tailwindcss(),
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+      },
+    }),
+    svgr(),
+  ],
 });
