a23dbe4512ae5eb93d167a39030dd79937ad8760
maksbubata <maks1412@gmail.com>
2025-02-13 22:20:02 +0200

    fixed links

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -2,7 +2,6 @@ import { useState, useRef, useEffect } from "react";
 
 const Links = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const dropdownRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
@@ -14,56 +13,54 @@ const Links = () => {
         setIsOpen(false);
       }
     };
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => document.removeEventListener("mouseup", handleClickOutside);
   }, []);
 
   return (
-    <div className="relative flex justify-start ">
+    <div className="relative flex justify-start">
       <div className="relative" ref={dropdownRef}>
         <button
           onClick={() => setIsOpen(!isOpen)}
-          className="px-4 py-2  cursor-pointer rounded-md flex flex-row"
+          className="px-4 py-2 cursor-pointer rounded-md flex flex-row"
         >
-          <img
-            src="/assets/links.svg"
-            alt="linksSocials"
-            className="w-5 h-5"
-          ></img>
+          <img src="/assets/links.svg" alt="linksSocials" className="w-5 h-5" />
           Links
         </button>
 
         {isOpen && (
-          <>
-            <div className="absolute left-2 mt-2 w-40 bg-black shadow-lg rounded-md">
-              <a
-                href="https://github.com/maksmarinov/wordlereact"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-2 px-4 py-2 hover:bg-violet-400"
-              >
-                <img
-                  src="/assets/github.svg"
-                  alt="GitHub"
-                  className="w-5 h-5"
-                />{" "}
-                GitHub
-              </a>
-              <a
-                href="https://www.linkedin.com/in/maksim-marinov-694b3853/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="flex items-center gap-2 px-4 py-2 text-white-500 fill-current hover:bg-violet-400"
-              >
-                <img
-                  src="/assets/linkedin.svg"
-                  alt="LinkedIn"
-                  className="w-5 h-5"
-                />{" "}
-                LinkedIn
-              </a>
+          <div className="absolute left-full ml-2 top-0 w-40 bg-black shadow-lg rounded-md flex flex-col">
+            <div
+              onClick={() =>
+                window.open(
+                  "https://github.com/maksmarinov/wordlereact",
+                  "_blank"
+                )
+              }
+              onMouseUp={(e) => e.stopPropagation()}
+              className="flex items-center gap-2 px-4 py-2 w-full hover:bg-violet-400 cursor-pointer block"
+            >
+              <img src="/assets/github.svg" alt="GitHub" className="w-5 h-5" />
+              GitHub
             </div>
-          </>
+            <div
+              onClick={() =>
+                window.open(
+                  "https://www.linkedin.com/in/maksim-marinov-694b3853/",
+                  "_blank"
+                )
+              }
+              onMouseUp={(e) => e.stopPropagation()}
+              className="flex items-center gap-2 px-4 py-2 w-full hover:bg-violet-400 cursor-pointer block"
+            >
+              <img
+                src="/assets/linkedin.svg"
+                alt="LinkedIn"
+                className="w-5 h-5"
+              />
+              LinkedIn
+            </div>
+          </div>
         )}
       </div>
     </div>
