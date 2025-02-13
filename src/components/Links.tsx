76b8d37a867c19821e83a010e8ffff83cab35150
maksbubata <maks1412@gmail.com>
2025-02-13 22:25:38 +0200

    removed blocks

--- src/components/Links.tsx
+++ src/components/Links.tsx
@@ -38,7 +38,7 @@ const Links = () => {
                 )
               }
               onMouseUp={(e) => e.stopPropagation()}
-              className="flex items-center gap-2 px-4 py-2 w-full hover:bg-violet-400 cursor-pointer block"
+              className="flex items-center gap-2 px-4 py-2 w-full hover:bg-violet-400 cursor-pointer "
             >
               <img src="/assets/github.svg" alt="GitHub" className="w-5 h-5" />
               GitHub
@@ -51,7 +51,7 @@ const Links = () => {
                 )
               }
               onMouseUp={(e) => e.stopPropagation()}
-              className="flex items-center gap-2 px-4 py-2 w-full hover:bg-violet-400 cursor-pointer block"
+              className="flex items-center gap-2 px-4 py-2 w-full hover:bg-violet-400 cursor-pointer "
             >
               <img
                 src="/assets/linkedin.svg"
